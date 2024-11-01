030549fc4623fa4606a5fef03e0854648c8204fb
aman <amansharma0603@gmail.com>
2024-11-01 23:42:15 +1100

    rebasing

--- network_monitor/simulate/icmp_simulation.py
+++ network_monitor/simulate/icmp_simulation.py
@@ -30,7 +30,5 @@ target_port = 80
 while True:
     packet = IP(src=spoofed_ip, dst=target_ip)/TCP(dport=target_port, flags="S")
     send(packet, count=10)# Send 10 packets
-<<<<<<< HEAD
     time.sleep(1)
-=======
->>>>>>> a3865a7935e816c75d480cfdca03ac488f63b57a
+

d20f5f292fdd2410ed5db24b9be4e5cfa6e1a86c
Aman <165676586+amancodeshere@users.noreply.github.com>
2024-11-04 14:39:46 +1100

    refactor

--- network_monitor/geo_ip.py
+++ network_monitor/geo_ip.py
@@ -12,8 +12,6 @@ def is_private_ip(ip):
     Returns:
         bool: True if the IP is in a private range, False if it is not.
     """
-    # This function simply calls the is_private method on the ipaddress
-    # object, which checks if the IP is in a private range.
     return ipaddress.ip_address(ip).is_private
 
 def get_geo_location(ip):
@@ -26,14 +24,12 @@ def get_geo_location(ip):
     Returns:
         data: The geolocation data or a failure message if the IP address is in a private range.
     """
-    # Check if the IP address is in a private range
     if is_private_ip(ip):
         return {'status': 'fail', 'message': 'private range', 'query': ip}
 
     # request the IP-API service to get the geolocation
     response = requests.get(f"http://ip-api.com/json/{ip}")
 
-    # Parse the JSON-response to a dictionary
     data = response.json()
     # print(f"Geo-IP response for {ip}: {data}")
 

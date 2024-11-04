1b5b687a2ae3e272e6265deeabea5e01d6767715
Aman <165676586+amancodeshere@users.noreply.github.com>
2024-11-04 14:42:46 +1100

    refac

--- network_monitor/config.py
+++ network_monitor/config.py
@@ -1,7 +1,6 @@
 # config.py
-ALERT_THRESHOLD = 2  # Number of suspicious packets to trigger an alert
-BLACKLISTED_IPS = ['10.168.201.65', "127.0.0.1:8000", "192.168.1.10", "8.8.8.8", "127.0.0.1", "17.248.155.203", '127.0.0.1', '1.1.1.1', '208.67.222.222' ,'192.168.1.100']  # Sample blacklisted IPs
 
-allowed_countries = ['Australia', 'United States', 'United Kingdom']  # Example list
-
-
+# Sample blacklisted IPs
+BLACKLISTED_IPS = ['10.168.201.65', "127.0.0.1:8000", "192.168.1.10", "8.8.8.8", "127.0.0.1", "17.248.155.203", '127.0.0.1', '1.1.1.1', '208.67.222.222' ,'192.168.1.100']
+# Example list
+allowed_countries = ['Australia', 'United States', 'United Kingdom']
